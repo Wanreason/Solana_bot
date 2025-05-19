dbd8db724e858801911066dfe16f91fbff5a7db6
Wanreason <realmallon25@gmail.com>
2025-05-19 01:57:12 +0000

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-MIN_VOLUME = 5000  # Minimum token volume in USD to trigger alerts
+MIN_VOLUME = 5000      # Minimum trading volume to consider (USD)
+MIN_LIQUIDITY = 5000   # Minimum liquidity pool value to consider (USD)
