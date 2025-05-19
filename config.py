0f1663cd17fe34b441100b77bc3de68ac710e040
Wanreason <realmallon25@gmail.com>
2025-05-19 01:54:10 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+MIN_VOLUME = 5000  # Minimum token volume in USD to trigger alerts
